768c7205a60eb8c533e467f813b335dd46c7b208
Vlad Hartmann <artmannv@yahoo.co.uk>
2013-10-20 23:57:39 +0100

    Added calls to storage API

--- Android/assets/www/js/functions.js
+++ Android/assets/www/js/functions.js
@@ -8,7 +8,7 @@ function showHideSearchBar() {
 function createNavigation(folders) {
 	var output = '<ul data-role="listview">';
 	output += '<li data-role="list-divider" class="list-divider"></li>';
-	output += '<li data-icon="plus" id="addFolderBtn"><a href="#">New Folder</a></li>';
+	output += '<li data-icon="plus"><a href="javascript:addFolder()">New Folder</a></li>';
 	output += listFolders(folders);
 	output += '<li data-role="list-divider" class="list-divider"></li>';
 	output += '<li data-icon="gear"><a href="#">Settings</a></li>';
@@ -40,3 +40,70 @@ function listArticles(articles) {
 	output += '</ul>';
 	$('#articles').html(output);
 }
+
+
+
+function addFolder() {
+	$.mobile.changePage('#addFolder', {
+        transition : 'pop',
+        role : 'dialog'
+    });
+}
+
+function saveFolder() {
+	$.mobile.changePage('#main');
+}
+
+function cancelFolder() {
+	$.mobile.changePage('#main');
+}
+
+function saveArticle() {
+	$.mobile.changePage('#main');
+}
+
+function cancelArticle() {
+	$.mobile.changePage('#main');
+}
+
+function addArticle() {
+	$.mobile.changePage('#addArticle');
+}
+
+function saveArticle(db, articleData) {
+	articleData.title = $('#articleTitle').val();
+	articleData.description = $("#articleDescription").val();
+	db.transaction(function(tx) {
+		tx.executeSql("INSERT INTO Articles (title, description) VALUES (?, ?)",[articleData.title, articleData.description])
+		}, onDbError, onDbSuccess);
+	$.mobile.changePage('#main');
+}
+
+function cancelArticle() {
+	$.mobile.changePage('#main');
+}
+
+function onDbError(error) {
+	alert("Database error: " + error.message);
+}
+
+function onDbSuccess(transaction, results) {
+	console.log("Database call successfull.");
+}
+
+function getArticles(tx) {
+	tx.executeSql("CREATE TABLE IF NOT EXISTS Articles (title, description)");
+	tx.executeSql("SELECT * FROM Articles", [], onSelectedArticlesSuccess, onDbError);
+}
+
+function onSelectedArticlesSuccess(transaction, results) {
+	dbresults = results;
+	var len = results.rows.length;
+	$('#articles').append('<ul data-role="listview">');
+	for (var i = 0; i < len; i++) {
+		$('#articles').append('<li><a href="#">' + results.rows.item(i).title + '</a></li>');
+	}
+	$('#articles').append('</ul>');
+	$("#notelist").listview("refresh");
+}
+
